426b1237293475c178d26ee358259f1d0dbf3cf0
assemblyDruid <cfarvin@gmail.com>
2018-09-23 14:24:31 -0400

    now compiling with linux toolchain

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -g lexer.c -o lexer -Wall -pedantic -Werror -std=c89
--- lexer.c
+++ lexer.c
@@ -4,17 +4,25 @@
 #include <math.h>
 #include <assert.h>
 
-#ifndef mlstring // multi_line_string
-#define mlstring(x) (#x)
-#endif // mlstring
+#ifndef __GNUC__
+#deinfe __attribute__(x) /*nul*/
+#endif
+
+#ifndef mlstring /* mlstring -> "multi line string" */
+#define mlstring(x) ("#x")
+#endif
 
 #ifndef TokenStreamSize
 #define TokenStreamSize (sizeof(TokenStream)/sizeof(TokenStream[0]))
-#endif // TokenStreamSize
+#endif
+
+#ifndef assemblyDruid_todo
+#define assemblyDruid_todo printf("TODO\n"); fflush(stdout); assert(0)
+#endif
 
 typedef enum
     {
-	terminal_type_none, // [ cfarvin::TODO ] remove this? probably.
+	terminal_type_none,
 	terminal_type_unknown,
 	terminal_type_terminal,
 	terminal_type_nonterminal
@@ -22,7 +30,7 @@ typedef enum
 
 typedef enum
     {
-	token_type_none, // [ cfarvin::TODO ] remove this? probably.
+	token_type_none,
 	token_type_unknown,
 	token_type_keyword,
 	token_type_numeric,
@@ -40,71 +48,71 @@ typedef struct
     char* data;
 } Token;
 
-Token TokenStream[100]; // [ cfarvin::NOTE::TODO ] default intialization len for now
+Token TokenStream[100];
 
-// [ cfarvin::TODO ]
+/* [ cfarvin::TODO ] */
 /* struct LEXER_STATE */
 /* { */
 /*     uint64_t char_position; */
 /* } LexerState; */
 
-//[ cfarvin:TODO ]
+/* [ cfarvin:TODO ] */
 /* struct STATISTICS */
 /* { */
 /* } statistics; */
 
-const char* sample_program = mlstring(
-#summon stdio
+/* const char* sample_program = mlstring( */
+/*     \#summon stdio */
     
-    status entry_point()
-    {
-	if(g:count)
-	{
-	    // do stuff
-	    /* do more stuff */
-	}
+/*     status entry_point() */
+/*     { */
+/* 	if(g:count) */
+/* 	{ */
+/* 	    /\* do stuff *\/ */
+/* 	    /\* do more stuff *\/ */
+/* 	} */
     
-	stdout("Hello world!\n");
-	return(status:success);
-    }
-    );
+/* 	stdout("Hello world!\n"); */
+/* 	return(status:success); */
+/*     } */
+/*     ); */
+
+/* function prototypes */
+void ExpandTokenStream();
+const char* const  NextToken();
+
+const char IsAlphabetical(const char character);
+const char IsNumeric(const char character);
+const char IsDelimiter(const char character);
+
+const char TestIsAlphabetical();
  
-//
-// function_prototypes
-//
-void ExpandTokenStream(); // [ cfarvin::TODO ]
-const char* NextToken();
-
-inline const char IsAlphabetical(const char character);
-inline const char IsNumeric(const char character);
-// inline const char IsDelimiter(const char character);
-
-inline const char IsKeyword(const char* p_character); // [ cfarvin::TODO ] string internment
-inline const char IsOperator(const char*);
-inline const char IsForbidden(const char*);
-
-inline const char IsTerminal(const char*);
-inline const char IsNonTerminal(const char*);
-
-const TokenType DetermineTokenType();
-const TerminalType DeterminTerminalType();
+const char IsKeyword(const char* p_character); /* [ cfarvin::TODO ] string internment */
+const char IsOperator(const char*);
+const char IsForbidden(const char*);
+ 
+const char IsTerminal(const char*);
+const char IsNonTerminal(const char*);
+TokenType DetermineTokenType();
+TerminalType DeterminTerminalType();
 
 
-const char* NextToken()
+const char* const NextToken()
 {
-    static uint8_t times_expanded = 0;
+    /* static uint8_t times_expanded = 0; */
 
-    // [ cfarvin::REMEMBER::TODO ]
-    // plan is to raise to power of <times_expanded> for memory allocation
+    /* 
+       plan is to raise to power of <times_expanded> for memory allocation
+    */
 
     return 0;
 }
 
-inline const char IsAlphabetical(const char character)
+const char IsAlphabetical(const char character)
 {
     if (
-	(character > 64) && (character < 91)  || // ASCII 'A' -> 'Z'
-	(character > 96) && (character < 123)    // ASCII 'a' -> 'z'
+	((character > 64) && (character < 91))  || /* ASCII 'A' -> 'Z' */
+	((character > 96) && (character < 123))    /* ASCII 'a' -> 'z' */
 	)
     {
 	return 1;
@@ -113,9 +121,9 @@ inline const char IsAlphabetical(const char character)
     return 0;
 }
 
-inline const uint8_t  TestIsAlphabetical()
+const char TestIsAlphabetical()
 {
-    uint8_t test_results = 1;
+    char test_results = 1;
 
     char alpha = 'A';
     for (; alpha < 'Z'; alpha++)
@@ -142,10 +150,10 @@ inline const uint8_t  TestIsAlphabetical()
     return 1;
 }
 
-inline const char IsNumeric(const char character)
+const char IsNumeric(const char character)
 {
     if (
-	(character > 47) && (character < 58) // ASCII '0' -> '9'
+	(character > 47) && (character < 58) /* ASCII '0' -> '9' */
 	)
     {
 	return 1;
@@ -154,7 +162,7 @@ inline const char IsNumeric(const char character)
     return 0;
 }
 
-inline const char TestIsNumeric()
+const char TestIsNumeric()
 {
     uint8_t test_results = 1;
 
@@ -172,7 +180,7 @@ inline const char TestIsNumeric()
     return 1;
 }
 
-inline const char IsDelimiter(const char character)
+const char IsDelimiter(const char character)
 {
     if(character == ' ' || 
        character == ';' || 
@@ -186,36 +194,82 @@ inline const char IsDelimiter(const char character)
     return 0; 
 }
 
-inline const char TestIsDelimiter()
+const char TestIsDelimiter()
 {
     uint8_t test_results = 1;
     char delimiters[] =
         {
-            ' ', // 0
-            ';', // 1
-            ',', // 2
-            '#', // 3
-            ':'  // 4
+            ' ', 
+            ';', 
+            ',', 
+            '#', 
+            ':'  
         };
 
     char delimiter = 0;
     for (; delimiter > 5; delimiter++)
     {
-        test_results = IsDelimiter(delimiters[delimiter]);
+        test_results = IsDelimiter(delimiters[(int) delimiter]);
         if (!test_results) { return 0; }
     }
 
     return 1;
 }
 
+const char IsNonTerminal(const char* character)
+{
+    assemblyDruid_todo;
+    return 0;
+}
+
+const char IsTerminal(const char* character)
+{
+    assemblyDruid_todo;
+    return 0;
+}
+
+const char IsForbidden(const char* character)
+{
+    assemblyDruid_todo;
+    return 0;
+}
+
+const char IsOperator(const char* character)
+{
+    assemblyDruid_todo;
+    return 0;
+}
+
+
+const char IsKeyword(const char* p_character)
+{
+    /* [ assemblyDruid::TODO ] string internment? */
+    assemblyDruid_todo;
+    return 0;
+}
+
+const char TestIsKeyword()
+{
+    assemblyDruid_todo; return 0;
+}
+
+const char TestIsOperator()
+{
+    assemblyDruid_todo; return 0;
+}
+
+const char TestIsForbidden()
+{
+    assemblyDruid_todo; return 0;
+}
+
 
 int main(int argc, char** argv)
 {
+    /* Token tempToken; */
     assert(TestIsAlphabetical());
     assert(TestIsNumeric());
     assert(TestIsDelimiter());
-
-    Token tempToken; // the "in progress" token as we iterate through the char stream.
     
     printf("\n[ SUCCESS ]\n");
     return(0);
