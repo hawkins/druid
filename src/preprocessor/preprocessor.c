6c51c235f857f4b64c4d59ccbfef39765e268621
assemblyDruid <cfarvin@gmail.com>
2018-10-15 13:50:16 -0400

    moving to alternate workstation

--- src/preprocessor/preprocessor.c
+++ src/preprocessor/preprocessor.c
@@ -15,7 +15,7 @@
 #define DRUID_DEBUG 1
 #endif // DRUID_DEBUG
 
-#if WIN32 || __MINGW32__
+#if _WIN32 || __MINGW32__
 #ifndef dbgPrint
 #if DRUID_DEBUG 
 #define dbgPrint(_str, ...) printf(_str, __VA_ARGS__)
@@ -49,7 +49,6 @@ typedef enum
 typedef struct
 {
     PreprocessingDirective directive;
-    char* macro_name;
     char* macro_definition;
 } MacroCache;
 
@@ -138,15 +137,87 @@ MacroCache MacroCacheStream[100];
 //         fprintf(stderr, "Error: unknown preprocessor directive!"); 
 //         exit(1); 
 //     } 
-// } 
+// }
 
-// Maybe we call this repeatedly until no further directives are found?
-int main(int argc, char** argv)
+typedef struct
 {
+    char*  tome_file_buffer;
+    size_t tome_total_file_size;
+    FILE*  tome_file_stream;
+
+    char* tome_file_name;
+} TOME;
+TOME tome;
+
+void
+processTome()
+{
+    printf("[ processTome() ]\n");
+/*     // [ assemblyDruid::ASSUMPTION ] */
+/*     // the name of the executable will also be the name of the .tome file */
+/*     for () */
+/*     { */
+                            
+/*     } */
+
+/* #if _WIN32 */
+/* #pragma warning(push) */
+/*     // MSVC warns against <fopen> in favor of <fopen_s>. <fopen_s> is not xplatform */
+/* #pragma warning( disable: 4996 ) */
+/* #endif // _WIN32 */
+                        
+/*     tome_file_stream = fopen(, "r" ); */
+    
+/* #if _WIN32 */
+/* #pragma warning(push) */
+/* #pragma warning( disable: 4996 ) */
+/* #endif // _WIN32 */
+                        
+/*     if (tome_file_stream != NULL) */
+/*     { */
+/*         // Find the file len, allocate appropriate quantity of memory */
+/*         fseek(tome_file_stream, 0, SEEK_END); */
+/*         tome_total_file_size = ftell( tome_file_stream ); */
+/*         tome_file_buffer = (char*) calloc(tome_total_file_size, sizeof(char)); */
+/*         // Reset file position, read file contents into buffer */
+/*         fseek(tome_file_stream, 0, SEEK_SET); */
+/*         fread(tome_file_buffer, sizeof(char), tome_total_file_size, tome_file_stream); */
+/*     } */
+/*     else */
+/*     { */
+/*         // [ assemblyDruid::TODO ] unified system for error handling */
+/*         printf("[ FATAL ] Unable to locate <*.tome> file.\n"); */
+/*         assert(0); */
+/*     } */
+
+                        
+/*     printf("tome file:\n%s", tome_file_buffer); */
+}
+
+void
+extractTomeName(const char* argv0)
+{
+    printf("[ extractTomeName() ] argv0: %s\n", argv0);
+    
+}
+
+int
+main(int argc, char** argv)
+{
+
+    /* if(!(argc >= 2)) */
+    /* { */
+    /*     printf("Usage: <path-to-tome>"); */
+    /*     return -1; */
+    /* } */
+    
     for (int i = 0; i < argc; i++)
     {
         printf("argv[%d]: %s\n", i, argv[i]);
     }
+
+    extractTomeName(argv[0]);
+    processTome();
 
     //  Preprocesing assumptions (for now):
     //  1) A preprocessing directive takes up exactly one line, unless escaped (like C/C++).
@@ -159,9 +230,19 @@ int main(int argc, char** argv)
     char*  file_buffer;
     size_t total_file_size;
     FILE*  file_stream;
-    // [ assemblyDruid::TODO ] check all possible return values for fopen_s 
-    // [ assemblyDruid::TODO ] look into more than 'r' for better opening options 
+    // [ assemblyDruid::TODO ] check all possible return values for fopen
+#if _WIN32
+#pragma warning(push)
+    // MSVC warns against <fopen> in favor of <fopen_s>. <fopen_s> is not xplatform
+#pragma warning( disable: 4996 ) 
+#endif // _WIN32
+    
     file_stream = fopen(argv[1], "r" );
+    
+#if _WIN32
+#pragma warning(push)
+#pragma warning( disable: 4996 )    
+#endif // _WIN32
 
     if (file_stream != NULL)
     {
@@ -169,6 +250,7 @@ int main(int argc, char** argv)
         fseek(file_stream, 0, SEEK_END);
         total_file_size = ftell( file_stream );
         file_buffer = (char*) calloc(total_file_size, sizeof(char));
+        // Reset file position, read file contents into buffer
         fseek(file_stream, 0, SEEK_SET);
         fread(file_buffer, sizeof(char), total_file_size, file_stream);
     }
@@ -178,11 +260,9 @@ int main(int argc, char** argv)
         printf("[ FATAL ] Could not open the file.\n");
         assert(0);
     }
-    
 
     // Begin parsing the file_buffer for '#'
-    size_t cursor_position = 0;
-    for (; cursor_position < total_file_size; cursor_position++)
+    for (size_t cursor_position = 0; cursor_position < total_file_size; cursor_position++)
     {
         size_t directive_length = 0;
         char* directive_source = NULL;
@@ -251,21 +331,13 @@ int main(int argc, char** argv)
                     {
                         dbgPrint("[ DIRECTIVE DETECTED ] %s, %zd characters compared\n",
                                  directive_keyword, directive_source_cursor);
-
-                        /*
-                          !!!!!!!!!!!!!!!!!
-                          [ assemblyDruid::REMEMBER ]
-                          you were about to figure out the file that summon refers to,
-                          so that you can pass it to the commented function signature below. :)
-                          !!!!!!!!!!!!!!!!
-                        */
-                         
                         dbgPrint("\n");
+                        
                         /* void apply(FILE* fp, */
                         /*            long int offset, */
                         /*            PreprocessingDirective directive, */
                         /*            const char* parameter) */
-                            }
+                    }
                     break;
                 }
 
