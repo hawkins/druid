1ef9ab33ceab6a10908a59a7a084da68df32079b
Josh Hawkins <hawkins@users.noreply.github.com>
2018-10-18 20:19:21 -0400

    Add test/app switch to build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
-gcc -g druid.c -o druid -Wall -pedantic -D_TEST
+function compile_test {
+  gcc -g druid.c -o druid -Wall -pedantic -D_TEST
+}
+
+function compile_app {
+  gcc -g druid.c -o druid -Wall -pedantic -Werror
+}
+
+case "$1" in
+  "test")
+    compile_test ;;
+  "app")
+    compile_app  ;;
+  *)
+    echo "Usage: ./build.sh [test|app]"
+esac
+
